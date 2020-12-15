c8b52c210ec268c0bb6339f581d4015ec7a0f726
Rishab Jain <rishabjain1402@gmail.com>
2020-12-15 11:48:07 -0500

    Homework

---
+++ Homework/SimonSays/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "simonsays.c"
+#include "simonsays.h"
+
+int main()
+{
+    begingame();
+}
---
+++ Homework/SimonSays/simonsays.h
@@ -0,0 +1,47 @@
+/*simonsays.h*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int begingame(void);
+int question1(void);
+int question2(void);
+int question3(void);
+int question4(void);
+int question5(void);
+int question6(void);
+int question7(void);
+int question8(void);
+int question9(void);
+int question10(void);
+int question11(void);
+int question12(void);
+int question13(void);
+int question14(void);
+int question15(void);
+int question16(void);
+int question17(void);
+int question18(void);
+int question19(void);
+int question20(void);
+int question21(void);
+int question10(void);
+int question22(void);
+int question23(void);
+int question24(void);
+int question25(void);
+int question26(void);
+int question27(void);
+int question28(void);
+int question29(void);
+int question30(void);
+int question31(void);
+int question32(void);
+int question33(void);
+int question34(void);
+int question35(void);
+int question36(void);
+int question37(void);
+
